404da69ac7419b8bf94293fe615c4e9ae8a79f10
joakin <joaquin@chimeces.com>
2017-08-20 20:15:04 +0200

    Remove unnecessary port

--- src/Ports.elm
+++ src/Ports.elm
@@ -6,7 +6,4 @@ import Bookmarks exposing (BookmarkJSON)
 port save : ( String, String, List BookmarkJSON ) -> Cmd msg
 
 
-port saveLastUpdateTime : String -> Cmd msg
-
-
 port logOut : () -> Cmd msg
